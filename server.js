f01d5e052ccd1512324fda48927b50930e20b478
Adam Sackfield <adam@adamsackfield.uk>
2023-02-15 11:57:03 +0000

    initial server setup

---
+++ server.js
@@ -0,0 +1,5 @@
+const { PORT = 5550 } = process.env;
+const app = require('./app');
+const logger = require('./helpers/logger');
+
+app.listen(PORT, () => logger(`Server is running on PORT:${PORT}`, 'WARN'));
