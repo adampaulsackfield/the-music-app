e4c8824da1b751c977eeb4373cd89f395922c481
Constantin-Coica <constantin_marian.coica@yahoo.com>
2023-02-21 14:28:57 +0200

    Set up React boiler/componenets

---
+++ client/src/Components/SignUp/SignUp.jsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react'
+// Components
+
+// Styles
+import styles from './SignUp.scss'
+
+const SignUp = () => {
+
+  return <div>SignUp</div>;
+
+};
+
+export default SignUp
